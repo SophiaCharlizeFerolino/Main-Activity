b1e6beb0a491e6b1af8db560c4655af923c256a1
SophiaCharlizeFerolino <scsferolino@gmail.com>
2023-03-20 17:08:01 +0800

    230320

---
+++ indexActivity.js
@@ -0,0 +1,23 @@
+console.log('Hello, Welcome to De La Salle University!');
+
+let firstName = 'Sophia';
+let lastName = 'Ferolino';
+let age = 20;
+
+let hobbies = ['reading books', 'playing badminton', 'playing the piano', 'swimming', 'shopping'];
+
+let homeAddress = {
+	houseNumber:'B18 L37',
+	street:'Favali',
+	villageOrSubdivision:'Villa Hermosa',
+	barangay:'Buhay na Tubig',
+	city:'Imus',
+	province:'Cavite',
+	zipCode:4103
+};
+
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(hobbies);
+console.log(homeAddress);
